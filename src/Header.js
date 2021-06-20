54ac49580b1f21ab57433afa483bb0c6a1c4eb83
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-06-20 16:03:38 +0300

    lab 11 is completed

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+import './header.css';
+/***********************************************/
+//--------------------------------------------//
+import Logout from "./Logout"
+//--------------------------------------------//
+/***********************************************/
+class Header extends React.Component {
+  render() {
+    return(
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Logout />
+      </Navbar>
+    )
+  }
+}
+
+export default Header;
