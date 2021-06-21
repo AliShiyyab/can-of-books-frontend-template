84433699255fa463c5885a170a0eaf396a6e9cd7
AliShiyyab <alishiyyab1998@gmail.com>
2021-06-21 16:41:44 -0700

    Lab12

--- react-app/src/MyFavoriteBooks.js
+++ react-app/src/MyFavoriteBooks.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import './myFavoriteBooks.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import BookCorsal from './BookCorsal';
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      books:[],
+    }
+  }
+
+  async componentDidMount() {
+    const email = this.props.auth0.user.email;
+    const booksReq = await axios.get(`${process.env.REACT_APP}/books?email=${email}`);
+    await this.setState({books:booksReq.data});
+  }
+
   render() {
     return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+      <Row>
+      { this.props.auth0.isAuthenticated && 
+      <Col>
+          {
+          this.state.books.length > 0 &&
+          <div>
+          <BookCorsal books={this.state.books} />
+          </div>
+          }
+      </Col>
+    }
+    </Row>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
